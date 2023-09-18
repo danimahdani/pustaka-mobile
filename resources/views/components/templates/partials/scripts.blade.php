43611e051c93cf7e6c29ab3a5c98e3361e5bcf43
mahdanidn <mahdanidn@gmail.com>
2023-09-18 11:12:55 +0700

    :white_check_mark: setup layout

---
+++ resources/views/components/templates/partials/scripts.blade.php
@@ -0,0 +1,25 @@
+ <!-- General JS Scripts -->
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+ </script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+ </script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+ <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+ <script src="{{ asset('js/stisla.js') }}"></script>
+
+ <!-- JS Libraies -->
+
+
+ <!-- Template JS File -->
+ <script src="{{ asset('js/scripts.js') }}"></script>
+ <script src="{{ asset('js/custom.js') }}"></script>
+ <script src="{{ asset('js/page/modules-datatables.js') }}"></script>
+
+
+ <!-- Page Specific JS File -->
+ @stack('extra-scripts')
